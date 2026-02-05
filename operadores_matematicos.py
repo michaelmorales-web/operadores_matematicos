584c43c7880d355abc2050984959cc4f8c338e88
michaelmorales-web <michaelmorales@colegioguanenta.edu.co>
2026-02-05 15:08:47 -0500

    Se actualiza el operadores_matematicos  con análisis y diseño

--- operadores_matematicos.py
+++ operadores_matematicos.py
@@ -1,14 +1,14 @@
 # Programa para invertir un número entero de cuatro cifras
 
 # input
-n = input("Digite un número de 4 digitos")
+n = input("Digite un número de 4 digitos: ")
 n = int(n)
 
 #procesing
 r4 = n%10
 r3 = (n//10)%10
 r2 = (n//100)%10
-r1 = (n//100)%10
+r1 = (n//1000)%10
 
 ni= r4*1000 + r3*100 + r2*10 + r1
 
